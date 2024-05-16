1eae75e98e0fb7eaeb6e39c72afbb0c5f9287dda
Dong Van Duong <duong19120@gmail.com>
2024-05-16 23:10:03 +0700

    auth-component

--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -1,18 +1,19 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { TransitionProps } from '@mui/material/transitions';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import { Box, Tab, Tabs } from "@mui/material";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement<any, any>;
   },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
@@ -28,10 +29,16 @@ export default function DialogAuth() {
     setOpen(false);
   };
 
+  const [value, setValue] = React.useState(1);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <React.Fragment>
       <Button variant="contained" onClick={handleClickOpen}>
-       Login
+        Login
       </Button>
       <Dialog
         open={open}
@@ -39,13 +46,43 @@ export default function DialogAuth() {
         keepMounted
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
+        sx={{ padding: 0 }}
       >
-        <DialogTitle>{"Đăng nhập bằng tài khoản"}</DialogTitle>
+        <DialogTitle sx={{ minWidth: "500px", padding: 0 }}>
+          <Box sx={{ width: "100%", height: "100%" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              TabIndicatorProps={{
+                style: { display: "none" },
+              }}
+              textColor="primary"
+            >
+              <Tab
+                label="Đăng Nhập"
+                value={1}
+                className=" w-1/2 h-full "
+                sx={{
+                  ...(value === 1
+                    ? { backgroundColor: "#e7e7e7" }
+                    : { backgroundColor: "white" }),
+                  fontWeight: 600,
+                }}
+              />
+              <Tab
+                label="Đăng ký"
+                value={2}
+                className=" w-1/2 h-full"
+                sx={{
+                  backgroundColor: value === 2 ? "#e7e7e7" : "white",
+                  fontWeight: 600,
+                }}
+              />
+            </Tabs>
+          </Box>
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
+          <DialogContentText id="alert-dialog-slide-description"></DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Disagree</Button>
