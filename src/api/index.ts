719c36358160163536c572bbc83b09c01edb63a8
Dong Van Duong <duong19120@gmail.com>
2024-10-04 20:57:41 +0700

    feat: add webpack

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { InternalAxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
+
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import './assets/styles/index.css';
 import { router } from './routers/index';
 import 'react-toastify/dist/ReactToastify.css';
 import store from './stores/app/store';
